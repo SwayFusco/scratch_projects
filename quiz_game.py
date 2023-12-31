181413607c3449fab7cb4e6877785a96179545a0
SwayFusco <sway.inbox@gmail.com>
2023-12-31 14:16:44 -0700

    Added friendly comments and changed the header information to match the project

--- quiz_game.py
+++ quiz_game.py
@@ -1,21 +1,23 @@
 """
 Author:  Sway (bryant) Fusco
-Project: Week 3 Technical Assignment
-Version: 20231231-141343
-Cohort:  2023-11-30-de-eastern
-Teacher: Tiago Basil
-School:  Promineo Tech/ACC Data Engineering
+Project: Simple Quiz Game
+Version: 20231231-141614
 """
+# Welcome message
 print("Welcome to my computer quiz!")
 
+# Ask if the user wants to play
 playing = input("Do you want to play? ")
 
+# Check if the user wants to play, otherwise quit
 if playing.lower() != "yes":
     quit()
 
+# Inform that the game is starting
 print("Okay! Let's Play! :)")
 score = 0
 
+# Question 1
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print("Correct!")
@@ -23,6 +25,7 @@ if answer.lower() == "central processing unit":
 else:
     print("Incorrect")
 
+# Question 2
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphical processing unit":
     print("Correct!")
@@ -30,6 +33,7 @@ if answer.lower() == "graphical processing unit":
 else:
     print("Incorrect")
 
+# Question 3
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
     print("Correct!")
@@ -37,6 +41,7 @@ if answer.lower() == "random access memory":
 else:
     print("Incorrect")
 
+# Question 4
 answer = input("What does UPS stand for? ")
 if answer.lower() == "universal power supply":
     print("Correct!")
@@ -44,5 +49,6 @@ if answer.lower() == "universal power supply":
 else:
     print("Incorrect")
 
+# Display the final score
 print("You got " + str(score) + " question(s) correct!")
 print("You got " + str((score / 4) * 100) + "%.")
